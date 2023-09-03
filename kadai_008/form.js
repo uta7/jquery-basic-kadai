1f59e9ea7287236a1f530f5eb012e879a31987f9
uta7 <122001807+uta7@users.noreply.github.com>
2023-09-03 15:32:33 +0900

    Add files via upload

---
+++ kadai_008/form.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".btn").on('click', function () {
+        $("#ex").val("クリックしました！");
+        //input.textContent = 
+        //console.log($(".text-box").val());
+    });
+});
+
+
